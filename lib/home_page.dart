167662e8cb960298c49e6c5937d6b0c7170618ce
j-lim45 <crimson4511@gmail.com>
2025-08-18 20:46:20 +0800

    created app home page

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppHomePage extends StatefulWidget {
+  @override
+  State<AppHomePage> createState() => _AppHomePage();
+  
+}
+
+  class _AppHomePage extends State<AppHomePage> {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false, // this should work but it doesnt
+        home: Scaffold(
+          body: Center(child: Text("HELLO, HELLO"),)
+        )
+      );
+    }
+  }
+
