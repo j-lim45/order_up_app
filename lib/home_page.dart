0bd67f166d50e65e4a5836757c999f71620a5184
j-lim45 <crimson4511@gmail.com>
2025-08-22 00:51:04 +0800

    added ProductContainer class

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'database_service.dart';
 
-class AppHomePage extends StatefulWidget {
-  const AppHomePage({super.key});
+// This is a product container that should hold all important information about a product and it has its own
+// card displayed on the home page
+class ProductContainer extends StatelessWidget {
+  final String productId;
+  final String name;
+  final String imgUrl;
+  final double price;
+  final int quantity;
+
+  const ProductContainer({super.key, required this.productId, required this.name, required this.imgUrl, required this.price, required this.quantity});
 
   @override
-  State<AppHomePage> createState() => _AppHomePage();
-  
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        width: 150, height: 150,
+        child: Column(
+          children: [
+            Image.network(imgUrl, width: 80, height: 90,),
+            Text(productId),
+            Text("₱${price.toString()}"),
+            Text("Stock: ${quantity.toString()}")
+        ],)
+      )
+      , onTap: () => {},
+    );
+  }
 }
 
+
+
+Future<void> getProducts() async {
+
+  // DataSnapshot needs line 2 to function and holds a data from Firebase DB
+  DataSnapshot? snapshot = await DatabaseService().read(path: 'test/products');
+  print(snapshot?.value);
+}
 
 // Returns a container or a listing of a product with info such as product name, price, and quantity
 InkWell get containerProduct {
@@ -27,38 +63,52 @@ InkWell get containerProduct {
           Text("HELLo")
       ],)
     )
-    , onTap: () => {},
+    , onTap: () => {getProducts},
   );
 }
 
-  class _AppHomePage extends State<AppHomePage> {
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false, // this should work but it doesnt
-        home: Scaffold(
+// This widget is the first thing a user should see after logging in
+class AppHomePage extends StatefulWidget {
+  const AppHomePage({super.key});
 
-          // App Bar
-          appBar: AppBar(
-            backgroundColor: Colors.red,
-            title: Text("Hello")
-          ),
+  @override
+  State<AppHomePage> createState() => _AppHomePage();
+  
+}
 
-          // Body of the column of product listings
-          body: Center(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    containerProduct,
-                    containerProduct
-                ],)
-              ]
-            ,)
+class _AppHomePage extends State<AppHomePage> {
+  String debugg = '';
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // this should work but it doesnt
+      home: Scaffold(
+
+        // App Bar
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text("Hello")
+        ),
+
+        // Body of the column of product listings
+        body: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  containerProduct,
+                  containerProduct,
+              ],),
+              Text(debugg),
+            ]
           ,)
-        )
-      );
-    }
+        ,)
+      )
+    );
   }
+}
 
