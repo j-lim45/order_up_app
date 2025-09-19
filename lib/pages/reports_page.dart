004c72130dd80453598cd0c2e8db5203b4e2ea3e
j-lim45 <crimson4511@gmail.com>
2025-09-19 15:49:16 +0800

    added other navbar pages

---
+++ lib/pages/reports_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import '../backend/database_service.dart';
+import 'package:order_up_app/components/product_container.dart';
+import 'package:order_up_app/pages/home_page.dart';
+import 'package:order_up_app/components/bottom_nav_bar.dart';
+
+class ReportsPage extends StatefulWidget {
+  const ReportsPage({super.key});
+
+  @override
+  State<ReportsPage> createState() => _ReportsPage();
+}
+
+class _ReportsPage extends State<ReportsPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Reports Page")
+    );
+  }
+}
---
+++ lib/pages/stock_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import '../backend/database_service.dart';
+import 'package:order_up_app/components/product_container.dart';
+import 'package:order_up_app/pages/home_page.dart';
+import 'package:order_up_app/components/bottom_nav_bar.dart';
+
+class StockPage extends StatefulWidget {
+  const StockPage({super.key});
+
+  @override
+  State<StockPage> createState() => _StockPage();
+}
+
+class _StockPage extends State<StockPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Stock Page")
+    );
+  }
+}
