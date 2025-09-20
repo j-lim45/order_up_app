6554c1c348a759708a64f3188525afcbd7711101
j-lim45 <crimson4511@gmail.com>
2025-09-20 22:29:01 +0800

    Added class for products

---
+++ lib/backend/product_class.dart
@@ -0,0 +1,15 @@
+class Product {
+  String productId = "";
+  String productName = "";
+  String productImgUrl = "";
+  int quantity = 0;
+  double price = 0.0;
+
+  Product({
+    required this.productId, 
+    required this.productName, 
+    required this.productImgUrl, 
+    required this.quantity, 
+    required this.price
+  });
+}
