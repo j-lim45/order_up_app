5e514ba2077b9368602f9b71a22c0f5fa22d23be
j-lim45 <crimson4511@gmail.com>
2025-09-20 14:27:40 +0800

    unified element colors into one file

---
+++ lib/components/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color pitchColor = Color(0xFFFFEBD2);
+  static Color appBarColor = Color(0xFFFFE1BD);
+  static Color headerColor = Colors.amber;
+
+  static Color maroonColor = Color(0xFFAE3D33);
+  static Color lightMaroonColor = Color(0xFFEEB087);
+
+  static Color unselectedItem = const Color(0xFF9E9999);
+}
