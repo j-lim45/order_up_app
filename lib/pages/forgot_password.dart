0a05e63343541bb39259313cbee9c2f1183532a4
j-lim45 <you@example.com>
2025-09-26 14:09:02 +0800

    added forgot password page

---
+++ lib/pages/forgot_password.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatelessWidget {
+  const ForgotPassword({super.key});
+
+  Widget build(BuildContext build) {
+    return Scaffold(body: Text('ForgotPassword class'));
+  }
+}
