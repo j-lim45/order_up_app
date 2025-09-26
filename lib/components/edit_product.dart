c1a14a0f0047af13b7eee454269b494383f63791
j-lim45 <crimson4511@gmail.com>
2025-09-26 17:49:27 +0800

    added edit product widget

---
+++ lib/components/edit_product.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:order_up_app/components/app_colors.dart';
+
+class EditProduct extends StatelessWidget {
+  const EditProduct({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("Hello"));
+  }
+}
