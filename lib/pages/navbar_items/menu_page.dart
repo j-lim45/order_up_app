7c9d91e5c7bae46773c33c55233a58abbb54c834
j-lim45 <you@example.com>
2025-10-05 12:17:30 +0800

    removed import from missing file

--- lib/pages/navbar_items/menu_page.dart
+++ lib/pages/navbar_items/menu_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import '../../backend/database_service.dart';
-import 'package:order_up_app/components/product_container.dart';
 import 'package:order_up_app/pages/navbar_items/home_page.dart';
 import 'package:order_up_app/components/bottom_nav_bar.dart';
 
