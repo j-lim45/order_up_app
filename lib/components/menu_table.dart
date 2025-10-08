55eadda7ce66c739fea5f7d0750fd0bd3faa8abe
j-lim45 <you@example.com>
2025-10-08 23:49:06 +0800

    Menu Page

---
+++ lib/components/menu_table.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:order_up_app/components/app_colors.dart';
+import 'package:order_up_app/components/edit_product.dart';
+import 'package:order_up_app/components/add_sale.dart';
+import 'package:order_up_app/backend/product_class.dart';
+
+
+class MenuTable extends StatelessWidget {
+  final String category;
+
+  const MenuTable({super.key, required this.category});
+  
+  @override
+  Widget build(BuildContext context) {
+    // StreamBuilder is used to update the table everytime data is changed in realtime
+    return StreamBuilder<DatabaseEvent>(
+      stream: FirebaseDatabase.instance.ref("products").onValue,      // the current data of the products database
+      builder: (context, snapshot) {                                  // the actual widget where the data is to be placed in
+        if (snapshot.connectionState == ConnectionState.waiting) {    // loading indicator while waiting
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {                                      // error
+          return Center(child: Text("Error: ${snapshot.error}"));
+        }
+        if (!snapshot.hasData) {                                      // No products in table
+          return Center(child: Text("No products available"));
+        }
+
+        // Raw json data from products database
+        final data = snapshot.data!.snapshot.value as Map<Object?, Object?>;
+
+        final List<Container> productRows = [];
+
+        // Loops through each product in the table to add each attribute data into a DataRow
+        for (var product in data.entries) {
+          final productRow = product.value as Map<Object?, Object?>;
+
+          if (productRow['category'].toString()==category) {
+            Product currentProduct =  Product(
+              productId: product.key.toString(),
+              productImgUrl: productRow['image_url'].toString(),
+              productName: productRow['name'].toString(),
+              quantity: int.parse(productRow['quantity'].toString()),
+              price: double.parse(productRow['price'].toString()),
+              category: productRow['category'].toString(),
+              barcode: productRow['barcode_num'].toString()
+            );
+
+            productRows.add(
+              Container(
+                width: 350, height: 50,
+                margin: EdgeInsets.only(top: 2, bottom: 2),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), color: AppColors.greyContainer),
+                child: ListTile(
+                  leading: currentProduct.productImgUrl.toString() != "" ? 
+                  Image.network(
+                    currentProduct.productImgUrl.toString(),
+                    width: 40, height: 40
+                  ) : Image.asset('img/orderuplogo.png'),
+                  title: Text(currentProduct.productName),
+                  onTap: () => showDialog(
+                    context: context, 
+                    builder: (BuildContext context) {
+                      return AddSale(product: currentProduct);
+                    }
+                  ),
+                )
+              )
+            );
+          }
+        }
+        return Container(
+          width: 370, 
+          margin: EdgeInsets.only(bottom: 20),
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), color: Colors.white),
+          child: Center(
+            child: Column(
+              children: productRows
+            ),
+          )
+        );
+      },
+    );
+  }
+}
